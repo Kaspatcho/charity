f711f27745c7ee3833e6b541c94c6b4370f82429
Kaspatcho <nosilaw7777@gmail.com>
2024-10-12 13:29:41 -0300

    updating layout

--- resources/js/pages/Dashboard.jsx
+++ resources/js/pages/Dashboard.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 
 export default function Dashboard() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    Dashboard page!
-                </div>
-            </div>
+        <div>
+            Dashboard page!
         </div>
     );
 }
--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 
 export default function Home() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    Home page!
-                </div>
-            </div>
+        <div>
+            Home page!
         </div>
     );
 }
