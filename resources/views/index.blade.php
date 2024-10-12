2e40cf84f6d4c0571f6bc717ef2ca624d27cdb10
Kaspatcho <nosilaw7777@gmail.com>
2024-10-12 13:29:59 -0300

    updating default html configuration

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,14 @@
-<div id="app"></div>
-@viteReactRefresh
-@vite(['resources/js/app.js', 'resources/sass/app.scss'])
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    @viteReactRefresh
+    @vite(['resources/js/app.js', 'resources/sass/app.scss'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
