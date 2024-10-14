cb1a93d0275390467bb61f6595ca7894481da0c0
Kaspatcho <nosilaw7777@gmail.com>
2024-10-14 03:01:53 -0300

    refactoring zone code

--- resources/js/components/Progress.jsx
+++ resources/js/components/Progress.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-export default function Progress({ value, max=1, warningZone= 0.5, dangerZone = 0.3, mustGrow=false, children }) {
+export default function Progress({ value, max=1, warningZone= 0.4, dangerZone = 0.7, mustGrow=false, children }) {
     let width = `${value / max * 100}%`
     let backgroundWidth = `${100 - (value / max * 100)}%`
     let className = 'bg-success'
     let backgroundColor = 'darkgreen'
     let percentage = value / max
-    if((percentage < warningZone && mustGrow) || (percentage > warningZone && !mustGrow)) {
+    let inZone = (a, b) => a > b
+    if(mustGrow) {
+        inZone = (a, b) => a < b;
+        [ warningZone, dangerZone ] = [ 1 - warningZone, 1 - dangerZone ]
+    }
+
+    if(inZone(percentage, warningZone)) {
         className = 'bg-warning'
         backgroundColor = 'darkgoldenrod'
     }
 
-    if((percentage < dangerZone && mustGrow) || (percentage > dangerZone && !mustGrow)) {
+    if(inZone(percentage, dangerZone)) {
         className = 'bg-danger'
         backgroundColor = 'darkred'
     }
